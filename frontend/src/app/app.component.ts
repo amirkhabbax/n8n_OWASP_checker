c1267ee49d97172f2bf533d745dddbf04b840e93
Amirhossein <amir.khabbaz@gmail.com>
2026-08-30 20:26:06 +0330

    another test added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,4 +9,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(){
+        // INTENTIONALLY VULNERABLE — for security detector testing only.
+    const userInput = 'test';
+    const element = document.createElement('div');
+    element.outerHTML = userInput;
+  }
 }
