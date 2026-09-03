ad4c3de951cb783a2f0e1125fa2ce7f2395df000
Erfan <erfanscott2@gmail.com>
2026-09-03 22:27:16 +0330

    add 2 more vulnerabilities

--- backend/src/main/java/com/simpleblog/backend/controller/PostController.java
+++ backend/src/main/java/com/simpleblog/backend/controller/PostController.java
@@ -1,26 +1,31 @@
 package com.simpleblog.backend.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.simpleblog.backend.entity.Post;
 import com.simpleblog.backend.repository.PostRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
 
     private final PostRepository postRepository;
+    private final JdbcTemplate jdbcTemplate;
 
-    public PostController(PostRepository postRepository) {
+    public PostController(PostRepository postRepository, JdbcTemplate jdbcTemplate) {
         this.postRepository = postRepository;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @GetMapping
     public List<Post> getPosts() {
         return postRepository.findAll();
+    }
+
+    @PatchMapping
+    public void updatePostTitle(@RequestParam Long id, @RequestParam String title) {
+        jdbcTemplate.execute("update posts set title=" + title + "wehre id=" + id.toString());
     }
 }
