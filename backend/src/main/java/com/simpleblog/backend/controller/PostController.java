2656642140eb783cfac9059ec2d15bb5006797d2
Amirhossein <amir.khabbaz@gmail.com>
2026-09-04 14:18:15 +0330

    test

--- backend/src/main/java/com/simpleblog/backend/controller/PostController.java
+++ backend/src/main/java/com/simpleblog/backend/controller/PostController.java
@@ -1,11 +1,16 @@
 package com.simpleblog.backend.controller;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.simpleblog.backend.entity.Post;
 import com.simpleblog.backend.repository.PostRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -26,6 +31,7 @@ public class PostController {
 
     @PatchMapping
     public void updatePostTitle(@RequestParam Long id, @RequestParam String title) {
+        
         jdbcTemplate.execute("update posts set title=" + title + "wehre id=" + id.toString());
     }
 }
